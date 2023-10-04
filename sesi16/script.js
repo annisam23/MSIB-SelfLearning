be9eda3c6a02b1c0685f859d15823985b016894d
annisam23 <annisamrahma23@gmail.com>
2023-10-04 09:54:15 +0700

    sesi16

---
+++ sesi16/script.js
@@ -0,0 +1,8 @@
+const clickCounter = () => {
+    if(localStorage.clickcount){
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+    }else{
+        localStorage.clickcount =1;
+    }
+    document.getElementById("tes").innerHTML = localStorage.clickcount;
+}
